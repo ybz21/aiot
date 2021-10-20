c6a49accf772ea77be3ce22f1dbed40aaeee1ee7
颜丙政 <yanbingzheng@4paradigm.com>
2021-10-20 23:42:12 +0800

    add

--- car/decision/decision.py
+++ car/decision/decision.py
@@ -2,25 +2,29 @@ import gym
 from stable_baselines3 import PPO
 import highway_env
 import time
+import os
+
+current_dir = os.path.dirname(__file__)
 
 
 class Decision:
     def __init__(self):
-        self.model = PPO.load("highway_ppo/model")
-        obs = self.env.reset()
+        model_path = os.path.join(current_dir, 'highway_ppo/model')
+        self.model = PPO.load(model_path)
 
     def get_raw_action(self, obs):
         action, _ = self.model.predict(obs)
         return action
 
     def get_action(self, obs):
+        # https://highway-env.readthedocs.io/en/latest/actions/index.html#discrete-meta-actions
         action = self.get_raw_action(obs)
         if action == 0:
-            return 'changeLeft'
+            return 'changeLeft',action
         elif action == 2:
-            return 'changeRight'
+            return 'changeRight',action
         else:
-            return 'forward'
+            return 'forward',action
 
 
 def main():
--- car/main.py
+++ car/main.py
@@ -8,6 +8,24 @@ import time
 
 TIME_TURN = 1
 TIME_FORWARD = 1
+SPEED = 0.5
+USE_RULE_ACTION = True
+
+
+def get_rule_action(distance, now_lane):
+    if distance < 30.0 and now_lane == 'right':
+        return 'changeLeft'
+    elif distance < 30.0 and now_lane == 'left':
+        return 'changeRight'
+    else:
+        return 'forward'
+
+
+def get_rl_action(decision, distance, now_lane):
+    obs = generate_obs(distance, now_lane)
+    action, raw_action = decision.get_action(obs)
+    print(f'rl raw_action:{raw_action}')
+    return action
 
 
 def main():
@@ -16,14 +34,22 @@ def main():
     car = SpeedCar()
 
     while True:
-        distance = get_distance()
+        distances = []
+        for i in range(10):
+            distance = get_distance()
+            distances.append(distance)
 
-        obs = generate_obs(distance, now_lane)
+        distance = sum(distances) / 10.0
+        print("distance: %.1f cm" % distance)
 
-        action = decision.get_action(obs)
-        print(f'---distance: {distance}, action: {action}')
+        if USE_RULE_ACTION:
+            action = get_rule_action(distance, now_lane)
+        else:
+            action = get_rl_action(decision, distance, now_lane)
 
+        print(f'action: {action}')
         if action == 'changeLeft':
+            print('changeLeft')
             car.stop()
             car.left()
             time.sleep(TIME_TURN)
@@ -39,6 +65,7 @@ def main():
             now_lane = 'left'
 
         elif action == 'changeRight':
+            print('changeRight')
             car.stop()
             car.right()
             time.sleep(TIME_TURN)
@@ -53,7 +80,11 @@ def main():
             car.stop()
             now_lane = 'right'
         else:
+            print('forward')
             car.forward()
+            # time.sleep(1)
+
+        # time.sleep(5)
 
 
 if __name__ == '__main__':
